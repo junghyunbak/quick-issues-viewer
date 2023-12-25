afc8bd9fd08f29497cefd3a994235829ff42635f
junghyunbak <jeong5728@gmail.com>
2023-12-25 17:36:53 +0900

    chore: 절대경로 설정

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,16 @@
+const CracoAlias = require("craco-alias");
+
 module.exports = {
   babel: {
     presets: ["@emotion/babel-preset-css-prop"],
   },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        tsConfigPath: "tsconfig.paths.json",
+      },
+    },
+  ],
 };
