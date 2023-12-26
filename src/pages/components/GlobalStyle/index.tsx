d4405f4caa205334f23c3b59786bf614d7d98b25
junghyunbak <jeong5728@gmail.com>
2023-12-26 15:51:46 +0900

    style: 모바일 환경에서 루트 폰트 크기 설정

--- src/pages/components/GlobalStyle/index.tsx
+++ src/pages/components/GlobalStyle/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 // styles
 import { Global, css } from "@emotion/react";
+import { device } from "@/assets/styles";
 
 interface GlobalStyleProps {
   children: React.ReactNode;
@@ -12,6 +13,10 @@ const GlobalStyles = css`
   :root {
     font-size: 16px;
     box-sizing: border-box;
+
+    @media ${device.mobile} {
+      font-size: 14px;
+    }
   }
 
   p,
