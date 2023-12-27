750ec92974cce22b9822e8ce103cd4359dc0ddb2
junghyunbak <jeong5728@gmail.com>
2023-12-27 16:04:29 +0900

    refactor: 라벨 리스트 컴포넌트 위치 변경

--- src/pages/Main/components/IssueList/IssueListItem/index.tsx
+++ src/pages/Main/components/IssueList/IssueListItem/index.tsx
@@ -2,7 +2,7 @@
 import { css } from "@emotion/react";
 
 // components
-import { IssueListItemLabelList } from "@/pages/Main/components/IssueList/IssueListItemLabelList";
+import { IssueListItemLabelList } from "@/pages/Main/components/IssueList/IssueListItem/IssueListItemLabelList";
 
 import { type components } from "@octokit/openapi-types";
 
@@ -22,33 +22,6 @@ export function IssueListItem({ issue }: IssueListItemProps) {
         list-style: none;
       `}
     >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        `}
-      >
-        <img
-          src={user?.avatar_url}
-          css={css`
-            width: 40px;
-            height: 40px;
-            border-radius: 9999px;
-          `}
-          alt="avatar_image"
-        />
-
-        <p
-          css={css`
-            margin: 0;
-            font-size: 12px;
-          `}
-        >
-          {user?.login}
-        </p>
-      </div>
-
       <p>{title}</p>
 
       <IssueListItemLabelList labels={labels} />
