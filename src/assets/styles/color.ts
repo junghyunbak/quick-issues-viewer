9c4d4c7fa2fd6d6df07aec8bddb5713153500511
junghyunbak <jeong5728@gmail.com>
2023-12-28 16:15:45 +0900

    style: 검색 아이콘에 키워드 추가

--- src/assets/styles/color.ts
+++ src/assets/styles/color.ts
@@ -3,6 +3,8 @@ export const w = "rgb(255, 255, 255)";
 export const g100 = "rgb(246, 248, 250)";
 export const g200 = "rgb(208, 215, 222)";
 
+export const g600 = "rgb(101, 109, 118)";
+
 export const b = "rgb(0, 0, 0)";
 
 export const success = "rgb(26, 127, 55)";
--- src/components/widgets/Search/index.tsx
+++ src/components/widgets/Search/index.tsx
@@ -26,21 +26,54 @@ export function Search() {
         css={css`
           display: flex;
           align-items: center;
-          justify-content: center;
 
-          width: 2rem;
           height: 2rem;
 
           border-radius: ${size.BORDER_RADIUS}px;
           border: 1px solid ${color.g200};
 
-          background-color: ${color.g100};
+          overflow: hidden;
 
           cursor: pointer;
         `}
         onClick={handleSearchButtonClick}
       >
-        <Magnifier />
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+
+            height: 100%;
+
+            padding: 0 0.75rem;
+
+            border-right: 1px solid ${color.g200};
+          `}
+        >
+          <p
+            css={css`
+              font-size: 0.875rem;
+              color: ${color.g600};
+            `}
+          >
+            Find Repository
+          </p>
+        </div>
+
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            width: 2rem;
+            height: 100%;
+
+            background-color: ${color.g100};
+          `}
+        >
+          <Magnifier />
+        </div>
       </div>
 
       {modalIsOpen && <SearchModal setIsModalOpen={setIsModalOpen} />}
