d4ab820849c819c0963d9b3d23f5b0a9fcdd251c
junghyunbak <jeong5728@gmail.com>
2023-12-28 11:09:55 +0900

    chore: 이슈 리스트 api에 perPage옵션 추가 & 전체 이슈 개수 가져오는 api 제작

--- src/apis/methods/get.ts
+++ src/apis/methods/get.ts
@@ -1,3 +1,5 @@
+import { defaultValue } from "@/constants";
+
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit();
@@ -5,7 +7,8 @@ export const octokit = new Octokit();
 export const getRepoIssueList = async (
   owner: string = "",
   repo: string = "",
-  label: string
+  label: string,
+  perPage: number = defaultValue.DEFAULT_PER_PAGE
 ) => {
   const queries = [`type:issue`, `repo:${owner}/${repo}`];
 
@@ -17,9 +20,30 @@ export const getRepoIssueList = async (
     data: { items },
   } = await octokit.rest.search.issuesAndPullRequests({
     q: queries.join("+"),
+    per_page: perPage,
   });
 
   return items;
+};
+
+export const getRepoIssueListTotalCount = async (
+  owner: string = "",
+  repo: string = "",
+  label: string
+) => {
+  const queries = [`type:issue`, `repo:${owner}/${repo}`];
+
+  if (label) {
+    queries.push(`label:${label}`);
+  }
+
+  const {
+    data: { total_count },
+  } = await octokit.rest.search.issuesAndPullRequests({
+    q: queries.join("+"),
+  });
+
+  return total_count;
 };
 
 export const getRepoIssueLabelList = async (
--- src/constants/defaultValue.ts
+++ src/constants/defaultValue.ts
@@ -1,2 +1,4 @@
 export const DEFAULT_REPO_OWNER = "junghyunbak";
 export const DEFAULT_REPO_NAME = "github-issues-viewer";
+
+export const DEFAULT_PER_PAGE = 10;
