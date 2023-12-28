21e1040148ef60ba4fb6e8773173039c89da7cc7
junghyunbak <jeong5728@gmail.com>
2023-12-29 01:09:01 +0900

    feat: 자주 사용되는 고정 & 가변 요소 혼용 레이아웃 구현

---
+++ src/components/Layout/FixedAndVariableLayout/index.tsx
@@ -0,0 +1,66 @@
+// react
+import { useMemo } from "react";
+
+// styles
+import { css } from "@emotion/react";
+
+interface FixedAndVariableLayout {
+  fixedElement: React.ReactNode;
+
+  variableElement: React.ReactNode;
+
+  direction?: "row" | "column";
+
+  reverse?: boolean;
+}
+
+export function FixedAndVariableLayout({
+  fixedElement,
+  variableElement,
+  direction = "row",
+  reverse = false,
+}: FixedAndVariableLayout) {
+  const flexDirection = useMemo(() => {
+    let tmp = direction;
+
+    if (reverse) {
+      tmp += "-reverse";
+    }
+
+    return tmp;
+  }, [direction, reverse]);
+
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: ${flexDirection};
+
+        overflow: auto;
+
+        width: 100%;
+        height: 100%;
+      `}
+    >
+      <div
+        css={css`
+          flex-basis: content;
+
+          overflow: auto;
+        `}
+      >
+        {fixedElement}
+      </div>
+
+      <div
+        css={css`
+          flex: 1;
+
+          overflow: auto;
+        `}
+      >
+        {variableElement}
+      </div>
+    </div>
+  );
+}
