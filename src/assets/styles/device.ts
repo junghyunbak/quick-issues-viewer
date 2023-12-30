8ac0b761ecf3a9c2b0f1cb0980a3b88565bdd9cb
junghyunbak <jeong5728@gmail.com>
2023-12-31 02:44:53 +0900

    chore: tablet 미디어 쿼리 수정

--- src/assets/styles/device.ts
+++ src/assets/styles/device.ts
@@ -2,9 +2,7 @@ import * as size from "./size";
 
 export const mobile = `(max-width: ${size.BREAKPOINT_TABLET}px)`;
 
-export const tablet = `(min-width: ${
-  size.BREAKPOINT_TABLET + 1
-}px) and (max-width: ${size.BREAKPOINT_PC}px)`;
+export const tablet = `(max-width: ${size.BREAKPOINT_PC}px)`;
 
 export const pc = `(min-width: ${size.BREAKPOINT_PC}px)`;
 
