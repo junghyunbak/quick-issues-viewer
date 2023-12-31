05ad759296abb2ea8750c1796dfd676cd3d93268
junghyunbak <jeong5728@gmail.com>
2023-12-31 19:07:03 +0900

    chore: default stale, cache time 변경

--- src/pages/components/ReactQueryProvider/index.tsx
+++ src/pages/components/ReactQueryProvider/index.tsx
@@ -8,7 +8,17 @@ const queryClient = new QueryClient({
     queries: {
       refetchOnWindowFocus: false,
       retry: 0,
-      staleTime: 1000 * 20,
+      /**
+       * - 이슈
+       * - 레포지토리
+       * - 라벨
+       * - 사용자
+       *
+       * 이슈 리스트를 제외하고는 자주 변경되지 않는 값이기 때문에
+       * default staleTime, cacheTime을 infinity로 설정
+       */
+      staleTime: Infinity,
+      cacheTime: Infinity,
       useErrorBoundary: true,
     },
   },
