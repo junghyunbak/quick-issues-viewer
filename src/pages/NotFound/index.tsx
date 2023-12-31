3dc79dad94f5447029b39dbd2246a4a5c398fae2
junghyunbak <jeong5728@gmail.com>
2023-12-31 19:16:02 +0900

    chore: 404 페이지 헤더 제거, 멘트 한글로 수정

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,10 +2,6 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
-// components
-import { Header } from "@/components/widgets/Header";
-import { FixedAndVariableLayout } from "@/components/Layout/FixedAndVariableLayout";
-
 // styles
 import { css } from "@emotion/react";
 import { color, size } from "@/assets/styles";
@@ -18,67 +14,53 @@ export function NotFound() {
   }, [navigate]);
 
   return (
-    <FixedAndVariableLayout
-      direction="column"
-      fixedElement={
-        <div
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+
+        padding: 2.5rem 1rem;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          gap: 0.5rem;
+        `}
+      >
+        <h1>Ooops!</h1>
+
+        <p
           css={css`
-            border-bottom: 1px solid ${color.g200};
+            color: ${color.inactive};
+            font-size: 1.5rem;
           `}
         >
-          <Header />
-        </div>
-      }
-      variableElement={
-        <div
+          페이지를 찾을 수 없습니다.
+        </p>
+
+        <button
+          type="button"
           css={css`
-            display: flex;
-            justify-content: center;
+            padding: 0.3125rem 1rem;
 
-            padding: 2.5rem 1rem;
+            border: 1px solid ${color.g200};
+            border-radius: ${size.BORDER_RADIUS}px;
+
+            background-color: ${color.g100};
+
+            font-weight: 500;
+            color: ${color.active};
+
+            cursor: pointer;
           `}
+          onClick={handleGoHomeButtonClick}
         >
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-              align-items: flex-start;
-              gap: 0.5rem;
-            `}
-          >
-            <h1>Ooops!</h1>
-
-            <p
-              css={css`
-                color: ${color.inactive};
-                font-size: 1.5rem;
-              `}
-            >
-              Page Not Found
-            </p>
-
-            <button
-              type="button"
-              css={css`
-                padding: 0.3125rem 1rem;
-
-                border: 1px solid ${color.g200};
-                border-radius: ${size.BORDER_RADIUS}px;
-
-                background-color: ${color.g100};
-
-                font-weight: 500;
-                color: ${color.active};
-
-                cursor: pointer;
-              `}
-              onClick={handleGoHomeButtonClick}
-            >
-              go home
-            </button>
-          </div>
-        </div>
-      }
-    />
+          go home
+        </button>
+      </div>
+    </div>
   );
 }
