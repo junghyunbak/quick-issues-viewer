8bce34a3992d389bb9ed83a10c28a0b8585faf79
junghyunbak <jeong5728@gmail.com>
2024-01-03 16:42:56 +0900

    chore: login 페이지 퍼블리싱, useQuery 사용하여 코드 개편

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,14 @@
 // react
-import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
+import { useQuery } from "react-query";
 
 // utils
 import queryString from "query-string";
+
+// styles
+import { css } from "@emotion/react";
+import { color } from "@/assets/styles";
 
 // apis
 import axios from "axios";
@@ -13,17 +18,35 @@ export function Login() {
 
   const { code } = queryString.parse(searchParams.toString());
 
-  useEffect(() => {
-    axios.post("/api/oauth/login", { code }).then(() => {
-      const redirectFrom = localStorage.getItem("redirect_from");
+  useQuery(
+    ["oauth", "login", code],
+    async () => {
+      await axios.post("/api/oauth/login", { code });
+    },
+    {
+      onSuccess() {
+        const redirectFrom = localStorage.getItem("redirect_from");
 
-      window.location.href = redirectFrom || "/";
-    });
-  }, [code]);
+        window.location.href = redirectFrom || "/";
+      },
+    }
+  );
 
   return (
-    <div>
-      <p>login...</p>
+    <div
+      css={css`
+        position: absolute;
+        inset: 0;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      <h3>Login...</h3>
+
+      <ThreeDots color={color.g600} />
     </div>
   );
 }
