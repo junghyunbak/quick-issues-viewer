fb8423c0df1da82f1d8c152431c47f7da492d695
junghyunbak <jeong5728@gmail.com>
2024-01-04 17:13:31 +0900

    refactor: 이슈 아이템 컴포넌트에서 comment  컴포넌트 분리

---
+++ src/pages/Main/components/IssueList/IssueListItem/IssueComment/index.tsx
@@ -0,0 +1,224 @@
+// react
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import { vs } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+// styles
+import { css } from "@emotion/react";
+import { color, device, size } from "@/assets/styles";
+
+// svgs
+import { ReactComponent as Reference } from "@/assets/svgs/reference.svg";
+
+// components
+import { FixedAndVariableLayout } from "@/components/Layout/FixedAndVariableLayout";
+
+// apis
+import { type components } from "@octokit/openapi-types";
+
+interface IssueCommentProps {
+  markdownText: string;
+
+  issueUrl: string;
+
+  user: components["schemas"]["nullable-simple-user"];
+}
+
+export function IssueComment({
+  markdownText,
+  issueUrl,
+  user,
+}: IssueCommentProps) {
+  return (
+    <FixedAndVariableLayout
+      fixedElement={
+        <a
+          css={css`
+            display: block;
+
+            width: 2.5rem;
+            height: 2.5rem;
+
+            border-radius: 9999px;
+            border: 1px solid ${color.g200};
+
+            overflow: hidden;
+
+            @media ${device.mobile} {
+              display: none;
+            }
+          `}
+          target="__blank"
+          href={user?.html_url}
+        >
+          <img
+            src={user?.avatar_url}
+            css={css`
+              width: 100%;
+              height: 100%;
+            `}
+            alt="issue-writer-profile"
+          />
+        </a>
+      }
+      variableElement={
+        <div
+          css={css`
+            padding-left: 1rem;
+
+            @media ${device.mobile} {
+              padding-left: 0;
+            }
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+            `}
+          >
+            <div
+              css={css`
+                display: flex;
+                justify-content: space-between;
+
+                background-color: ${color.g100};
+
+                padding: 0.5rem 0.75rem;
+
+                border: 1px solid ${color.g200};
+                border-top-left-radius: ${size.BORDER_RADIUS}px;
+                border-top-right-radius: ${size.BORDER_RADIUS}px;
+
+                position: relative;
+
+                &::before {
+                  content: " ";
+
+                  display: block;
+
+                  position: absolute;
+                  top: 0.6875rem;
+                  left: -0.5rem;
+
+                  width: 0.5rem;
+                  height: 1rem;
+
+                  clip-path: polygon(0 50%, 100% 0, 100% 100%);
+
+                  background-color: ${color.g200};
+                }
+
+                &::after {
+                  content: " ";
+
+                  display: block;
+
+                  position: absolute;
+                  top: 0.6875rem;
+                  left: -0.5rem;
+
+                  margin-left: 0.125rem;
+
+                  width: 0.5rem;
+                  height: 1rem;
+
+                  clip-path: polygon(0 50%, 100% 0, 100% 100%);
+
+                  background-color: ${color.g100};
+                }
+
+                @media ${device.mobile} {
+                  &::before,
+                  &::after {
+                    display: none;
+                  }
+                }
+              `}
+            >
+              <a
+                css={css`
+                  font-size: 0.875rem;
+                  font-weight: 600;
+
+                  color: ${color.b};
+                  text-decoration: none;
+
+                  &:hover {
+                    color: ${color.active};
+                    text-decoration: underline;
+                  }
+                `}
+                href={user?.html_url}
+                target="__blank"
+              >
+                {user?.login}
+              </a>
+
+              <a href={issueUrl} target="__blank">
+                <Reference />
+              </a>
+            </div>
+
+            <div
+              css={css`
+                border: 1px solid ${color.g200};
+                border-top: 0;
+                border-bottom-left-radius: ${size.BORDER_RADIUS}px;
+                border-bottom-right-radius: ${size.BORDER_RADIUS}px;
+
+                padding: 0.75rem;
+              `}
+            >
+              <Markdown
+                css={css`
+                  li {
+                    list-style: initial;
+                  }
+
+                  pre {
+                    padding: 0;
+                  }
+                `}
+                className="markdown-body"
+                remarkPlugins={[remarkGfm]}
+                rehypePlugins={[rehypeRaw]}
+                components={{
+                  code({ inline, children, className, node, ...props }) {
+                    const match = /language-(\w+)/.exec(className || "");
+
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        {...props}
+                        PreTag="div"
+                        language={match[1]}
+                        style={vs}
+                        customStyle={{
+                          background: "none",
+                          border: 0,
+                          margin: 0,
+                          fontSize: "1rem",
+                          lineHeight: "normal",
+                        }}
+                      >
+                        {String(children).replace(/\n$/, "")}
+                      </SyntaxHighlighter>
+                    ) : (
+                      <code {...props} className={className}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              >
+                {markdownText}
+              </Markdown>
+            </div>
+          </div>
+        </div>
+      }
+    />
+  );
+}
--- src/pages/Main/components/IssueList/IssueListItem/index.tsx
+++ src/pages/Main/components/IssueList/IssueListItem/index.tsx
@@ -1,19 +1,13 @@
 // react
 import React, { useCallback, useMemo } from "react";
-import Markdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
-import { vs } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 // components
 import { IssueListItemLabelList } from "@/pages/Main/components/IssueList/IssueListItem/IssueListItemLabelList";
-import { FixedAndVariableLayout } from "@/components/Layout/FixedAndVariableLayout";
+import { IssueComment } from "@/pages/Main/components/IssueList/IssueListItem/IssueComment";
 
 // svgs
 import { ReactComponent as IssueOpened } from "@/assets/svgs/issue-opened.svg";
 import { ReactComponent as IssueClosed } from "@/assets/svgs/issue-closed.svg";
-import { ReactComponent as Reference } from "@/assets/svgs/reference.svg";
 import { ReactComponent as PrOpen } from "@/assets/svgs/pr-open.svg";
 import { ReactComponent as PrClosed } from "@/assets/svgs/pr-closed.svg";
 import { ReactComponent as PrMerged } from "@/assets/svgs/pr-merged.svg";
@@ -21,7 +15,7 @@ import { ReactComponent as Comment } from "@/assets/svgs/comment.svg";
 
 // styles
 import { css } from "@emotion/react";
-import { color, device, size } from "@/assets/styles";
+import { color, device } from "@/assets/styles";
 import "github-markdown-css";
 
 // apis
@@ -243,193 +237,10 @@ export function IssueListItem({
             padding: 0.75rem;
           `}
         >
-          <FixedAndVariableLayout
-            fixedElement={
-              <a
-                css={css`
-                  display: block;
-
-                  width: 2.5rem;
-                  height: 2.5rem;
-
-                  border-radius: 9999px;
-                  border: 1px solid ${color.g200};
-
-                  overflow: hidden;
-
-                  @media ${device.mobile} {
-                    display: none;
-                  }
-                `}
-                target="__blank"
-                href={user?.html_url}
-              >
-                <img
-                  src={user?.avatar_url}
-                  css={css`
-                    width: 100%;
-                    height: 100%;
-                  `}
-                  alt="issue-writer-profile"
-                />
-              </a>
-            }
-            variableElement={
-              <div
-                css={css`
-                  padding-left: 1rem;
-
-                  @media ${device.mobile} {
-                    padding-left: 0;
-                  }
-                `}
-              >
-                <div
-                  css={css`
-                    display: flex;
-                    flex-direction: column;
-                  `}
-                >
-                  <div
-                    css={css`
-                      display: flex;
-                      justify-content: space-between;
-
-                      background-color: ${color.g100};
-
-                      padding: 0.5rem 0.75rem;
-
-                      border: 1px solid ${color.g200};
-                      border-top-left-radius: ${size.BORDER_RADIUS}px;
-                      border-top-right-radius: ${size.BORDER_RADIUS}px;
-
-                      position: relative;
-
-                      &::before {
-                        content: " ";
-
-                        display: block;
-
-                        position: absolute;
-                        top: 0.6875rem;
-                        left: -0.5rem;
-
-                        width: 0.5rem;
-                        height: 1rem;
-
-                        clip-path: polygon(0 50%, 100% 0, 100% 100%);
-
-                        background-color: ${color.g200};
-                      }
-
-                      &::after {
-                        content: " ";
-
-                        display: block;
-
-                        position: absolute;
-                        top: 0.6875rem;
-                        left: -0.5rem;
-
-                        margin-left: 0.125rem;
-
-                        width: 0.5rem;
-                        height: 1rem;
-
-                        clip-path: polygon(0 50%, 100% 0, 100% 100%);
-
-                        background-color: ${color.g100};
-                      }
-
-                      @media ${device.mobile} {
-                        &::before,
-                        &::after {
-                          display: none;
-                        }
-                      }
-                    `}
-                  >
-                    <a
-                      css={css`
-                        font-size: 0.875rem;
-                        font-weight: 600;
-
-                        color: ${color.b};
-                        text-decoration: none;
-
-                        &:hover {
-                          color: ${color.active};
-                          text-decoration: underline;
-                        }
-                      `}
-                      href={user?.html_url}
-                      target="__blank"
-                    >
-                      {user?.login}
-                    </a>
-
-                    <a href={html_url} target="__blank">
-                      <Reference />
-                    </a>
-                  </div>
-
-                  <div
-                    css={css`
-                      border: 1px solid ${color.g200};
-                      border-top: 0;
-                      border-bottom-left-radius: ${size.BORDER_RADIUS}px;
-                      border-bottom-right-radius: ${size.BORDER_RADIUS}px;
-
-                      padding: 0.75rem;
-                    `}
-                  >
-                    <Markdown
-                      css={css`
-                        li {
-                          list-style: initial;
-                        }
-
-                        pre {
-                          padding: 0;
-                        }
-                      `}
-                      className="markdown-body"
-                      remarkPlugins={[remarkGfm]}
-                      rehypePlugins={[rehypeRaw]}
-                      components={{
-                        code({ inline, children, className, node, ...props }) {
-                          const match = /language-(\w+)/.exec(className || "");
-
-                          return !inline && match ? (
-                            <SyntaxHighlighter
-                              {...props}
-                              PreTag="div"
-                              language={match[1]}
-                              style={vs}
-                              customStyle={{
-                                background: "none",
-                                border: 0,
-                                margin: 0,
-                                fontSize: "1rem",
-                                lineHeight: "normal",
-                              }}
-                            >
-                              {String(children).replace(/\n$/, "")}
-                            </SyntaxHighlighter>
-                          ) : (
-                            <code {...props} className={className}>
-                              {children}
-                            </code>
-                          );
-                        },
-                      }}
-                    >
-                      {body || ""}
-                    </Markdown>
-                  </div>
-                </div>
-              </div>
-            }
+          <IssueComment
+            user={user}
+            issueUrl={html_url}
+            markdownText={body || ""}
           />
         </div>
       )}
