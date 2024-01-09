6a617ca30de6ca32d851257a754154836ef3c26e
junghyunbak <jeong5728@gmail.com>
2024-01-09 21:56:51 +0900

    refactor: emtion/styled 설치 및 issueHeader에 적용

---
+++ src/pages/Main/components/IssueList/IssueListItem/IssueListItemHeader/index.styles.tsx
@@ -0,0 +1,105 @@
+// styles
+import styled from "@emotion/styled";
+import { color, zIndex, device } from "@/assets/styles";
+
+// svgs
+import { ReactComponent as IssueOpenedIcon } from "@/assets/svgs/issue-opened.svg";
+import { ReactComponent as IssueClosedIcon } from "@/assets/svgs/issue-closed.svg";
+import { ReactComponent as PrOpenIcon } from "@/assets/svgs/pr-open.svg";
+import { ReactComponent as PrClosedIcon } from "@/assets/svgs/pr-closed.svg";
+import { ReactComponent as PrMergedIcon } from "@/assets/svgs/pr-merged.svg";
+import { ReactComponent as CommentIcon } from "@/assets/svgs/comment.svg";
+
+export const PrOpen = styled(PrOpenIcon)`
+  fill: ${color.success};
+`;
+
+export const PrClosed = styled(PrClosedIcon)`
+  fill: rgb(207, 34, 46);
+`;
+
+export const PrMerged = styled(PrMergedIcon)`
+  fill: ${color.complete};
+`;
+
+export const IssueOpened = styled(IssueOpenedIcon)`
+  fill: ${color.success};
+`;
+
+export const IssueClosed = styled(IssueClosedIcon)`
+  fill: ${color.complete};
+`;
+
+export const Comment = styled(CommentIcon)`
+  fill: gray;
+`;
+
+export const HeaderLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0.5rem;
+
+  position: sticky;
+  top: 0;
+  z-index: ${zIndex.ISSUE_ITEM_HEADER};
+
+  background-color: ${color.w};
+
+  border-bottom: 1px solid ${color.g200};
+
+  &:hover {
+    @media ${device.canHover} {
+      background-color: ${color.g100};
+    }
+  }
+
+  cursor: pointer;
+`;
+
+export const IssueInfoLayout = styled.div`
+  display: flex;
+
+  gap: 0.5rem;
+`;
+
+export const IssueInfoStatusBox = styled.div`
+  padding-left: 0.25rem;
+`;
+
+export const IssueInfoTextBoxLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const IssueInfoTitleLayout = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.375em;
+`;
+
+export const IssueInfoTitleParagraph = styled.p`
+  font-weight: 600;
+  line-height: 1.5rem;
+`;
+
+export const IssueInfoSubTitleParagraph = styled.p`
+  color: gray;
+  font-size: 0.75rem;
+  line-height: 1.5rem;
+`;
+
+export const CommentCountingBox = styled.div`
+  display: flex;
+  align-items: top;
+  gap: 0.2rem;
+
+  padding-right: 0.25rem;
+`;
+
+export const CommentCountParagraph = styled.p`
+  white-space: nowrap;
+  font-size: 0.75rem;
+  color: gray;
+`;
--- src/pages/Main/components/IssueList/IssueListItem/IssueListItemHeader/index.tsx
+++ src/pages/Main/components/IssueList/IssueListItem/IssueListItemHeader/index.tsx
@@ -6,17 +6,8 @@ import { IssueSelectionStateContext } from "../../index.context";
 // components
 import { IssueListItemHeaderLabelList } from "./IssueListItemHeaderLabelList";
 
-// svgs
-import { ReactComponent as Comment } from "@/assets/svgs/comment.svg";
-import { ReactComponent as IssueOpened } from "@/assets/svgs/issue-opened.svg";
-import { ReactComponent as IssueClosed } from "@/assets/svgs/issue-closed.svg";
-import { ReactComponent as PrOpen } from "@/assets/svgs/pr-open.svg";
-import { ReactComponent as PrClosed } from "@/assets/svgs/pr-closed.svg";
-import { ReactComponent as PrMerged } from "@/assets/svgs/pr-merged.svg";
-
 // styles
-import { css } from "@emotion/react";
-import { color, device, zIndex } from "@/assets/styles";
+import * as S from "./index.styles";
 
 export function IssueListItemHeader() {
   const { id, state, title, comments, created_at, pull_request } =
@@ -37,163 +28,46 @@ export function IssueListItemHeader() {
   const StatusIcon = useMemo<React.ReactNode>(() => {
     if (pull_request) {
       if (state === "open") {
-        return (
-          <PrOpen
-            css={css`
-              path {
-                fill: ${color.success};
-              }
-            `}
-          />
-        );
+        return <S.PrOpen />;
       } else {
         if (!pull_request.merged_at) {
-          return (
-            <PrClosed
-              css={css`
-                path {
-                  fill: rgb(207, 34, 46);
-                }
-              `}
-            />
-          );
+          return <S.PrClosed />;
         } else {
-          return (
-            <PrMerged
-              css={css`
-                path {
-                  fill: ${color.complete};
-                }
-              `}
-            />
-          );
+          return <S.PrMerged />;
         }
       }
     }
 
-    return state === "open" ? (
-      <IssueOpened
-        css={css`
-          path {
-            fill: ${color.success};
-          }
-        `}
-      />
-    ) : (
-      <IssueClosed
-        css={css`
-          path {
-            fill: ${color.complete};
-          }
-        `}
-      />
-    );
+    return state === "open" ? <S.IssueOpened /> : <S.IssueClosed />;
   }, [state, pull_request]);
 
+  const isCommentExist = comments > 0;
+
   return (
-    <div
-      css={css`
-        display: flex;
-        justify-content: space-between;
+    <S.HeaderLayout onClick={handleIssueItemClick}>
+      <S.IssueInfoLayout>
+        <S.IssueInfoStatusBox>{StatusIcon}</S.IssueInfoStatusBox>
 
-        padding: 0.5rem;
-
-        position: sticky;
-        top: 0;
-        background-color: ${color.w};
-        z-index: ${zIndex.ISSUE_ITEM_HEADER};
-
-        border-bottom: 1px solid ${color.g200};
-
-        &:hover {
-          @media ${device.canHover} {
-            background-color: ${color.g100};
-          }
-        }
-
-        cursor: pointer;
-      `}
-      onClick={handleIssueItemClick}
-    >
-      <div
-        css={css`
-          display: flex;
-
-          gap: 0.5rem;
-        `}
-      >
-        <div
-          css={css`
-            padding-left: 0.25rem;
-          `}
-        >
-          {StatusIcon}
-        </div>
-
-        <div>
-          <div
-            css={css`
-              display: flex;
-              align-items: center;
-              flex-wrap: wrap;
-              gap: 0.375em;
-            `}
-          >
-            <p
-              css={css`
-                font-weight: 600;
-                line-height: 1.5rem;
-              `}
-            >
-              {title}
-            </p>
+        <S.IssueInfoTextBoxLayout>
+          <S.IssueInfoTitleLayout>
+            <S.IssueInfoTitleParagraph>{title}</S.IssueInfoTitleParagraph>
 
             <IssueListItemHeaderLabelList />
-          </div>
+          </S.IssueInfoTitleLayout>
 
-          <div>
-            <p
-              css={css`
-                color: gray;
-                font-size: 0.75rem;
-                line-height: 1.5rem;
-              `}
-            >
-              {new Date(created_at).toLocaleString()}
-            </p>
-          </div>
-        </div>
-      </div>
+          <S.IssueInfoSubTitleParagraph>
+            {new Date(created_at).toLocaleString()}
+          </S.IssueInfoSubTitleParagraph>
+        </S.IssueInfoTextBoxLayout>
+      </S.IssueInfoLayout>
 
-      {comments > 0 && (
-        <div
-          css={css`
-            display: flex;
-            align-items: top;
-            gap: 0.2rem;
+      {isCommentExist && (
+        <S.CommentCountingBox>
+          <S.Comment />
 
-            padding-right: 0.25rem;
-          `}
-        >
-          <Comment
-            css={css`
-              path {
-                fill: gray;
-              }
-            `}
-          />
-
-          <p
-            css={css`
-              white-space: nowrap;
-              font-size: 0.75rem;
-              color: gray;
-            `}
-          >
-            {comments}
-          </p>
-        </div>
+          <S.CommentCountParagraph>{comments}</S.CommentCountParagraph>
+        </S.CommentCountingBox>
       )}
-    </div>
+    </S.HeaderLayout>
   );
 }
