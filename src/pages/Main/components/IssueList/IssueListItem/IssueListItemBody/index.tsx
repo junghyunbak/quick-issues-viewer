d0459cb3f6d4ff681a1b80a31985661da3b1e603
junghyunbak <jeong5728@gmail.com>
2024-01-09 16:18:07 +0900

    feat: 댓글 열기 버튼 구현

--- src/pages/Main/components/IssueList/IssueListItem/IssueListItemBody/index.tsx
+++ src/pages/Main/components/IssueList/IssueListItem/IssueListItemBody/index.tsx
@@ -1,5 +1,5 @@
 // react
-import { Fragment, useCallback } from "react";
+import { Fragment, useCallback, useState } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
@@ -9,13 +9,80 @@ import { CloseBodyButton } from "./CloseBodyButton";
 
 // styles
 import { css } from "@emotion/react";
-import { color } from "@/assets/styles";
+import { color, size } from "@/assets/styles";
 
 // apis
 import { type components } from "@octokit/openapi-types";
 
 // hooks
 import { useOctokit } from "@/hooks";
+
+interface CommentListProps {
+  issueNumber: number;
+
+  setSelectedIssueId: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+function CommentList({ issueNumber, setSelectedIssueId }: CommentListProps) {
+  const { owner, repo } = useParams();
+
+  const { apiService } = useOctokit();
+
+  const handleIssueBodyCloseButtonClick = useCallback(() => {
+    setSelectedIssueId(null);
+  }, [setSelectedIssueId]);
+
+  const comments = useQuery(
+    ["issue", "comment", owner, repo, issueNumber],
+    async () => {
+      const comments = apiService.getIssueComments(
+        owner || "",
+        repo || "",
+        issueNumber
+      );
+
+      return comments;
+    }
+  );
+
+  if (!comments.data || comments.data.length === 0) {
+    return null;
+  }
+
+  return (
+    <Fragment>
+      <ul
+        css={css`
+          width: 100%;
+        `}
+      >
+        {comments.data.map((comment) => {
+          const { id, body, html_url, user, reactions } = comment;
+
+          return (
+            <li key={id}>
+              <IssueComment
+                markdownText={body || ""}
+                issueUrl={html_url}
+                user={user}
+                reactions={reactions}
+              />
+            </li>
+          );
+        })}
+      </ul>
+
+      <div
+        css={css`
+          width: 100%;
+
+          border-top: 2px solid #d0d7de;
+        `}
+      />
+      <CloseBodyButton onClick={handleIssueBodyCloseButtonClick} />
+    </Fragment>
+  );
+}
 
 interface IssueListItemBodyProps {
   issue: components["schemas"]["issue"];
@@ -27,28 +94,13 @@ export function IssueListItemBody({
   issue,
   setSelectedIssueId,
 }: IssueListItemBodyProps) {
-  const { owner, repo } = useParams();
+  const { number, body, user, html_url, reactions, comments } = issue;
 
-  const { apiService } = useOctokit();
+  const [commentsIsOpen, setCommentsIsOpen] = useState(false);
 
-  const { number, body, user, html_url, reactions } = issue;
-
-  const comments = useQuery(
-    ["issue", "comment", owner, repo, number],
-    async () => {
-      const comments = apiService.getIssueComments(
-        owner || "",
-        repo || "",
-        number
-      );
-
-      return comments;
-    }
-  );
-
-  const handleIssueBodyCloseButtonClick = useCallback(() => {
-    setSelectedIssueId(null);
-  }, [setSelectedIssueId]);
+  const handleCommentsOpenButtonClick = useCallback(() => {
+    setCommentsIsOpen(true);
+  }, [setCommentsIsOpen]);
 
   return (
     <div
@@ -70,46 +122,48 @@ export function IssueListItemBody({
         reactions={reactions}
       />
 
-      {comments.data && comments.data.length > 0 ? (
-        <Fragment>
-          <ul
-            css={css`
-              width: 100%;
-            `}
-          >
-            {comments.data.map((comment) => {
-              const { id, body, html_url, user, reactions } = comment;
-
-              return (
-                <li key={id}>
-                  <IssueComment
-                    markdownText={body || ""}
-                    issueUrl={html_url}
-                    user={user}
-                    reactions={reactions}
-                  />
-                </li>
-              );
-            })}
-          </ul>
-
-          <div
-            css={css`
-              width: 100%;
-
-              border-top: 2px solid #d0d7de;
-            `}
-          />
-
-          <CloseBodyButton onClick={handleIssueBodyCloseButtonClick} />
-        </Fragment>
-      ) : (
+      {!commentsIsOpen && (
         <div
           css={css`
             width: 100%;
 
             border-top: 2px solid #d0d7de;
           `}
+        >
+          {comments > 0 && (
+            <div
+              css={css`
+                background-color: ${color.g100};
+
+                border: 1px solid ${color.g200};
+                border-radius: ${size.BORDER_RADIUS}px;
+
+                margin-top: 0.75rem;
+
+                cursor: pointer;
+
+                padding: 0.3125rem 1rem;
+              `}
+              onClick={handleCommentsOpenButtonClick}
+            >
+              <p
+                css={css`
+                  text-align: center;
+                  font-weight: 600;
+                  font-size: 0.875rem;
+                `}
+              >
+                Show comments
+              </p>
+            </div>
+          )}
+        </div>
+      )}
+
+      {commentsIsOpen && (
+        <CommentList
+          issueNumber={number}
+          setSelectedIssueId={setSelectedIssueId}
         />
       )}
     </div>
