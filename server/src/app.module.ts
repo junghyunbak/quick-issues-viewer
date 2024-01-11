772ba14e7c0fab61aef9cfa358454fe6e8703329
junghyunbak <jeong5728@gmail.com>
2024-01-11 15:50:45 +0900

    chore: 로그 관련 api 일시적으로 제거
    mongoose 교체를 위함

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OAuthModule } from './modules/oauth/oauth.module';
-import { LogModule } from './modules/log/log.module';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import * as Joi from 'joi';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
@@ -18,7 +15,6 @@ import * as Joi from 'joi';
       }),
     }),
     OAuthModule,
-    LogModule,
   ],
   controllers: [],
   providers: [],
--- server/src/modules/log/dto/LogCreate.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class LogCreateDto {
-  @IsString()
-  readonly logLevel: string;
-
-  @IsString()
-  readonly message: string;
-}
