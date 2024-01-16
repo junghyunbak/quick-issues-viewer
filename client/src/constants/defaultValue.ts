7ace82fbcb39e8648912315037231559f4c8affb
junghyunbak <jeong5728@gmail.com>
2024-01-16 17:15:48 +0900

    fix: union -> enum 타입 변환 시 반영되지 않은 부분 수정

--- client/src/constants/defaultValue.ts
+++ client/src/constants/defaultValue.ts
@@ -1,8 +1,15 @@
+import {
+  IssuesState,
+  IssuesSort,
+  IssuesSortDirection,
+} from "@/types/issueSearchOptions";
+
 export const REPO_OWNER = "junghyunbak";
 export const REPO_NAME = "github-issues-viewer";
 
 export const ISSUES_PER_PAGE = 25;
 export const ISSUES_PAGE = 1;
-export const ISSUES_STATE: IssuesState = "open";
-export const ISSUES_SORT: IssuesSort = "created";
-export const ISSUES_SORT_DIRECTION: IssuesSortDirection = "desc";
+export const ISSUES_STATE: IssuesState = IssuesState.open;
+export const ISSUES_SORT: IssuesSort = IssuesSort.created;
+export const ISSUES_SORT_DIRECTION: IssuesSortDirection =
+  IssuesSortDirection.desc;
