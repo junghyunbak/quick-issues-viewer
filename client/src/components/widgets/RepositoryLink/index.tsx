7647bc0a4f99126ffcd7eac6b8da2aca23fbe19d
junghyunbak <jeong5728@gmail.com>
2024-01-16 18:36:09 +0900

    style: 레포지토리 링크 컴포넌트 스타일 분리작업

--- client/src/components/widgets/RepositoryLink/index.tsx
+++ client/src/components/widgets/RepositoryLink/index.tsx
@@ -1,48 +1,34 @@
 // react
+import { Fragment } from "react";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 // styles
-import { css } from "@emotion/react";
-import { color, size } from "@/assets/styles";
+import * as S from "./index.styles";
 
 export function RepositoryLink() {
   const { owner, repo } = useParams();
 
   return (
-    <div
-      css={css`
-        display: flex;
-        align-items: center;
-
-        a {
-          font-size: 0.875rem;
-          text-decoration: none;
-          color: ${color.b};
-
-          padding: 0.375rem 0.5rem;
-
-          border-radius: ${size.BORDER_RADIUS}px;
-
-          &:hover {
-            background-color: ${color.g100};
-          }
-        }
-      `}
-    >
+    <Fragment>
       <Helmet>
         <title>Github Issues Viewer - {`${repo} (${owner})`}</title>
       </Helmet>
 
-      <a href={`https://github.com/${owner}`} target="__blank">
-        {owner}
-      </a>
+      <S.Layout>
+        <S.LinkAnchor href={`https://github.com/${owner}`} target="__blank">
+          {owner}
+        </S.LinkAnchor>
 
-      <span>/</span>
+        <span>/</span>
 
-      <a href={`https://github.com/${owner}/${repo}/issues`} target="__blank">
-        {repo}
-      </a>
-    </div>
+        <S.LinkAnchor
+          href={`https://github.com/${owner}/${repo}/issues`}
+          target="__blank"
+        >
+          {repo}
+        </S.LinkAnchor>
+      </S.Layout>
+    </Fragment>
   );
 }
