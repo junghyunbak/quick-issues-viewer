0ce9ff1fb2f75e43b503e67e2e56d79c3ff9dcff
junghyunbak <jeong5728@gmail.com>
2024-08-27 12:50:42 +0900

    chore: 변경된 기본 레포지토리 이름 수정

--- client/src/constants/defaultValue.ts
+++ client/src/constants/defaultValue.ts
@@ -5,7 +5,7 @@ import {
 } from "@/types/issueSearchOptions";
 
 export const REPO_OWNER = "junghyunbak";
-export const REPO_NAME = "github-issues-viewer";
+export const REPO_NAME = "quick-issues-viewer";
 
 export const ISSUES_PER_PAGE = 25;
 export const ISSUES_PAGE = 1;
